59344175c33fe6cc63f5c4fbebbc6e6253614def
gmanipon <gmanipon@jpl.nasa.gov>
2019-01-14 23:02:22 +0000

    increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='grq2',
-    version='1.0.1',
+    version='1.0.2',
     long_description='GeoRegionQuery REST API using ElasticSearch backend',
     packages=find_packages(),
     include_package_data=True,
