0f8262074d6541d5796aa539b585e7155f7d50da
Gerald Manipon <pymonger@gmail.com>
2019-02-07 19:47:14 +0000

    remove supervisor from required (not python3 compatible yet)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=['Flask', 'gunicorn', 'eventlet', 'supervisor', 'pymongo',
+    install_requires=['Flask', 'gunicorn', 'eventlet', 'pymongo',
                       'elasticsearch>=1.0.0,<2.0.0', 'requests', 'pyshp',
                       'shapely==1.5.15', 'Cython', 'Cartopy==0.13.1', 'redis',
                       'flask-restplus>=0.9.2']
