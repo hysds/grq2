58d2471e3d997489e055fcbadfe23a860cad4b88
Gerald Manipon <pymonger@gmail.com>
2019-05-28 14:14:52 +0000

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='grq2',
-    version='1.1.0',
+    version='1.1.1',
     long_description='GeoRegionQuery REST API using ElasticSearch backend',
     packages=find_packages(),
     include_package_data=True,
