6b38438594e67ad64b20549e6b97671ac0aff483
Gerald Manipon <pymonger@gmail.com>
2020-03-18 10:19:10 -0700

    HC-167: migrate to community-developed flask-restx (#15)
    * migrate to flask-restx
    
    * install flaskx from master for now

--- setup.py
+++ setup.py
@@ -14,5 +14,10 @@ setup(
     install_requires=['Flask', 'gunicorn', 'eventlet', 'pymongo',
                       'elasticsearch>=1.0.0,<2.0.0', 'requests', 'pyshp',
                       'shapely>=1.5.15', 'Cython>=0.15.1', 'Cartopy>=0.13.1',
-                      'redis', 'flask-restplus>=0.9.2', 'future>=0.17.1']
+                      # TODO: remove installation of master branch after new release of
+                      # flask-restx includes the fix referred to here:
+                      # https://github.com/python-restx/flask-restx/issues/85
+                      'flask-restx @ git+https://git@github.com/python-restx/flask-restx',
+                      #'redis', 'flask-restx>0.1.1', 'future>=0.17.1']
+                      'redis', 'future>=0.17.1']
 )
