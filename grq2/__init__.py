1e8090b28ec2f3f1389ec670ea8919a47e84c6f1
DustinKLo <dustinlo.pcc@gmail.com>
2020-03-29 13:23:50 -0700

    added flask-cors, will remove later

--- grq2/__init__.py
+++ grq2/__init__.py
@@ -6,12 +6,16 @@ from future import standard_library
 standard_library.install_aliases()
 
 from flask import Flask
+from flask_cors import CORS  # TODO: will remove this once we figure out the proper host for the UI
 
 from hysds_commons.elasticsearch_utils import ElasticsearchUtility
 
 
 app = Flask(__name__)
 app.config.from_pyfile('../settings.cfg')
+
+# TODO: will remove this when ready for actual release, need to figure out the right host
+CORS(app)
 
 # initializing connection to GRQ's Elasticsearch
 grq_es = ElasticsearchUtility(app.config['ES_URL'], app.logger)
