78629792a315c0794e6266e52e5d3e186bfb05ab
Gerald Manipon <pymonger@gmail.com>
2020-04-29 11:51:41 -0700

    support reverse proxy; fix API name (#25)

--- grq2/__init__.py
+++ grq2/__init__.py
@@ -11,7 +11,68 @@ from flask_cors import CORS  # TODO: will remove this once we figure out the pro
 from hysds_commons.elasticsearch_utils import ElasticsearchUtility
 
 
+class ReverseProxied(object):
+    '''Wrap the application in this middleware and configure the 
+    front-end server to add these headers, to let you quietly bind 
+    this to a URL other than / and to an HTTP scheme that is 
+    different than what is used locally.
+
+    In nginx:
+        location /myprefix {
+            proxy_pass http://127.0.0.1:8878;
+            proxy_set_header Host $host;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Scheme $scheme;
+            proxy_set_header X-Script-Name /myprefix;
+        }
+
+    In apache:
+        RewriteEngine on
+        RewriteRule "^/grq$" "/grq/" [R]
+        SSLProxyEngine on
+        ProxyRequests Off
+        ProxyPreserveHost Off
+        ProxyPass /grq/static !
+        ProxyPass /grq/ http://localhost:8878/
+        ProxyPassReverse /grq/ http://localhost:8878/
+        <Location /grq>
+            Header add "X-Script-Name" "/grq"
+            RequestHeader set "X-Script-Name" "/grq"
+            Header add "X-Scheme" "https"
+            RequestHeader set "X-Scheme" "https"
+        </Location>
+        Alias /grq/static/ /home/ops/sciflo/ops/grq2/grq2/static/
+        <Directory /home/ops/sciflo/ops/grq2/grq2/static>
+            Options Indexes FollowSymLinks
+            AllowOverride All
+            Require all granted
+        </Directory>
+
+    :param app: the WSGI application
+    '''
+
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
+        if script_name:
+            environ['SCRIPT_NAME'] = script_name
+            path_info = environ['PATH_INFO']
+            if path_info.startswith(script_name):
+                environ['PATH_INFO'] = path_info[len(script_name):]
+
+        scheme = environ.get('HTTP_X_SCHEME', '')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+        x_forwarded_host = environ.get('HTTP_X_FORWARDED_HOST', '')
+        if x_forwarded_host:
+            environ['HTTP_HOST'] = x_forwarded_host
+        return self.app(environ, start_response)
+
+
 app = Flask(__name__)
+app.wsgi_app = ReverseProxied(app.wsgi_app)
 app.config.from_pyfile('../settings.cfg')
 
 # TODO: will remove this when ready for actual release, need to figure out the right host
