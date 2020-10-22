1bcc1533b707a27c020be2f4f25359fa60217226
DustinKLo <dustinlo.pcc@gmail.com>
2020-10-22 11:40:04 -0700

    formatting change

--- grq2/__init__.py
+++ grq2/__init__.py
@@ -12,7 +12,8 @@ from grq2.es_connection import get_grq_es, get_mozart_es
 
 
 class ReverseProxied(object):
-    '''Wrap the application in this middleware and configure the 
+    """
+    Wrap the application in this middleware and configure the
     front-end server to add these headers, to let you quietly bind 
     this to a URL other than / and to an HTTP scheme that is 
     different than what is used locally.
@@ -49,7 +50,7 @@ class ReverseProxied(object):
         </Directory>
 
     :param app: the WSGI application
-    '''
+    """
 
     def __init__(self, app):
         self.app = app
