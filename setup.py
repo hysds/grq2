87b0b5ea71b74b15991bfd44f5e7bd8952d55af2
dustinklo <dustinlo.pcc@gmail.com>
2021-09-15 12:39:14 -0700

    updated version of flask and flask-restx
    commented out click b/c its not being used by the repo

--- setup.py
+++ setup.py
@@ -14,8 +14,9 @@ setup(
     install_requires=[
         # TODO: remove this pin on click once this celery issue is resolved:
         # https://github.com/celery/celery/issues/6768
-        'click>=7.0,<8.0',
-        'flask-restx>=0.4.0',
+        # 'click>=7.0,<8.0',  # 'click>=7.0,<8.0',  # don't think we need click here because its nto used by mozart
+        'Flask>2.0.0,<3.0.0',
+        'flask-restx>=0.5.1',
         'elasticsearch>=7.0.0,<7.14.0',
         'shapely>=1.5.15',
         'Cython>=0.15.1',
