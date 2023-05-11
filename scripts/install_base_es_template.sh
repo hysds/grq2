a09080c04f381a282f5507c2a44aed56b99c47b8
Mike Cayanan <michael.d.cayanan@jpl.nasa.gov>
2023-05-11 12:39:20 -0700

    pass in args

--- scripts/install_base_es_template.sh
+++ scripts/install_base_es_template.sh
@@ -4,4 +4,4 @@ BASE_PATH=$(cd "${BASE_PATH}"; pwd)
 export PYTHONPATH=$BASE_PATH/..:$PYTHONPATH
 
 # install ops and dev templates
-$BASE_PATH/install_base_es_template.py
+$BASE_PATH/install_base_es_template.py $*
